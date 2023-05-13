966242af128ae15ecfe4c785135f3524b47799e9
Xinyi Liao <78093922+lisaliao0817@users.noreply.github.com>
2023-05-13 13:34:42 -0700

    Add tailwind CSS configurations and set up app/document

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,36 +1,41 @@
-import styles from '../styles/globals.css'
-
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<h2>User Registration</h2>
-			<form id="registrationForm" action="#" method="post">
-				<div className={styles.formGroup}>
-					<label for="username">Username:</label>
-					<input type="text" id="username" name="username" required />
-				</div>
-				<div className={styles.formGroup}>
-					<label for="password">Password:</label>
-					<input type="password" id="password" name="password" required />
-				</div>
-				<div className={styles.formGroup}>
-					<button type="submit" className={styles.btn}>Register</button>
-				</div>
-			</form>
-			<h2>User Login</h2>
-			<form id="loginForm" action="#" method="post">
-				<div className={styles.formGroup}>
-					<label for="loginUsername">Username:</label>
-					<input type="text" id="loginUsername" name="loginUsername" required />
-				</div>
-				<div className={styles.formGroup}>
-					<label for="loginPassword">Password:</label>
-					<input type="password" id="loginPassword" name="loginPassword" required />
-				</div>
-				<div className={styles.formGroup}>
-					<button type="submit" className={styles.btn}>Login</button>
-				</div>
-			</form>
+		<div className="flex justify-center items-center h-screen bg-gray-100">
+			<div className="bg-white p-8 shadow-md rounded-lg">
+				<h2 className="text-2xl font-bold mb-4">Login</h2>
+				<form>
+					<div className="mb-4">
+						<label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+							Username:
+						</label>
+						<input
+							type="text"
+							id="username"
+							name="username"
+							className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+							Password:
+						</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-500"
+							required
+						/>
+					</div>
+					<button
+						type="submit"
+						className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring"
+					>
+						Login
+					</button>
+				</form>
+			</div>
 		</div>
 	)
 }
